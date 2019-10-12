b616a9200c97ae4ed9fd4395815d9615d254f43e
Sergey Skupow <sergskypow@gmail.com>
2019-10-12 12:45:35 +0300

    post user detection

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,21 @@
 const router = require("express").Router();
 const verify = require('./verifyToken');
+const User = require("../model/User");
 
-router.get("/",verify, (req, res) => {
-  res.json({
-    posts: { title: "my first post", description: "random sdf dsafsasdfasfd" }
-  });
+router.get("/",verify, async (req, res) => {
+  const token = req.header('auth-token');
+  const id = req.user._id;
+  try{
+    const user = await User.findOne({ _id: id });
+    res.json({
+      posts: { title: "my first post", description: "random sdf dsafsasdfasfd",id:id,
+    user:user.email}
+    });
+  
+}catch(err){
+    res.status(400).send('invalid token')
+}
+  
 });
 
 module.exports = router;
